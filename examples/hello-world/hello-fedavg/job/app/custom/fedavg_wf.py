7ac1b5d666150011e2d2ac5b17fd07006741b477
chesterxgchen <chesterxgchen@yahoo.com>
2023-12-22 21:01:34 -0800

    fedAvg

--- examples/hello-world/hello-fedavg/job/app/custom/fedavg_wf.py
+++ examples/hello-world/hello-fedavg/job/app/custom/fedavg_wf.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from net import Net
 from nvflare.app_common.abstract.fl_model import FLModel, ParamsType
 from nvflare.app_common.utils.fl_model_utils import FLModelUtils
@@ -10,7 +12,8 @@ class FedAvg(WF):
     def __init__(self,
                  min_clients: int,
                  output_path: str,
-                 num_rounds: int
+                 num_rounds: int,
+                 metrics_keys: List[str] = None
                  ):
         super(FedAvg, self).__init__()
 
@@ -21,12 +24,19 @@ class FedAvg(WF):
         # (1) init flare_comm
         self.flare_comm.init(self)
         self.best_model = None
+        self.metrics_keys = metrics_keys if metrics_keys else ["accuracy", "auc",  "loss", "running_loss"]
 
     def run(self):
         net = Net()
         model = FLModel(params=net.state_dict(), params_type=ParamsType.FULL)
+        target_metrics = {}
 
+        # todo: add earlier stopping rules
         for current_round in range(0, self.num_rounds):
+
+            if self.stop_cond(model.metrics, target_metrics):
+                return
+
             print(f"Round {current_round}/{self.num_rounds} started.")
 
             print(f"Scatter and Gather")
@@ -64,3 +74,35 @@ class FedAvg(WF):
 
     def best_model_fn(self, model) -> FLModel:
         pass
+
+    def stop_cond(self, metrics: dict, target_metrics: dict):
+        if target_metrics is None:
+            return False
+
+        key = "accuracy"
+        if key in target_metrics:
+            value = metrics.get(key, -100)
+            target_value = target_metrics.get(key, -1)
+            if value >= target_value:
+                return True
+
+        key = "auc"
+        if key in target_metrics:
+            value = metrics.get(key, -100)
+            target_value = target_metrics.get(key, -1)
+            if value >= target_value:
+                return True
+
+        key = "loss"
+        if key in target_metrics:
+            value = metrics.get(key, 1e+10)
+            target_value = target_metrics.get(key, -1)
+            if value <= target_value:
+                return True
+
+        key = "running_loss"
+        if key in target_metrics:
+            value = metrics.get(key, 1e+10)
+            target_value = target_metrics.get(key, -1)
+            if value <= target_value:
+                return True
